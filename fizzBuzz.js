4fe59369eb1bd4c4ab36611f5d1dac3854c04421
Jacob Puckettgit config --global user.email jjp10015@gmail.com <jjp10015@gmail.com>
2023-01-25 08:54:12 -0700

    added fizzbuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,11 +2,13 @@
 
 
 
-for (let num = 1; i <= 50; num++){
+for (let num = 1; num <= 50; num++){
 
 
-    
-    if( num % 3 === 0 ) {
+     if (num % 3 === 0 && num % 5 === 0){
+        console.log("fizzbuzz")
+    }
+    else if( num % 3 === 0 ) {
         console.log("fizz")
     }
     else if( num % 5 === 0 ) {
