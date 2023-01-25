f4a514879dbe51db2d506b0c7804d78b1b035d21
Jacob Puckettgit config --global user.email jjp10015@gmail.com <jjp10015@gmail.com>
2023-01-25 08:19:14 -0700

    daily challenge

---
+++ fizzBuzz.js
@@ -0,0 +1,17 @@
+// Write your solution below this line:
+
+
+
+for (let i = 1; i <= 50; i++){
+
+
+    
+    if( num % 3 === 0 ) {
+        console.log(fizz)
+    }
+    else if( num % 5 === 0 ) {
+        console.log(buzz)
+    }else{
+        console.log(i)
+    }
+}
