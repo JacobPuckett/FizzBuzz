94f62bc37d1c8ab8a90d76842ae427a9f98e671d
Jacob Puckettgit config --global user.email jjp10015@gmail.com <jjp10015@gmail.com>
2023-01-25 08:31:44 -0700

    changed num to i

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -6,11 +6,11 @@ for (let i = 1; i <= 50; i++){
 
 
     
-    if( num % 3 === 0 ) {
-        console.log(fizz)
+    if( i % 3 === 0 ) {
+        console.log("fizz")
     }
-    else if( num % 5 === 0 ) {
-        console.log(buzz)
+    else if( i % 5 === 0 ) {
+        console.log("buzz")
     }else{
         console.log(i)
     }
