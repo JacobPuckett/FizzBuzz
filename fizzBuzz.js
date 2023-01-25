7617f3cb905cfd920f5432de57d8af016f09213d
Jacob Puckettgit config --global user.email jjp10015@gmail.com <jjp10015@gmail.com>
2023-01-25 08:43:57 -0700

    change veriable to num

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,16 +2,16 @@
 
 
 
-for (let i = 1; i <= 50; i++){
+for (let num = 1; i <= 50; num++){
 
 
     
-    if( i % 3 === 0 ) {
+    if( num % 3 === 0 ) {
         console.log("fizz")
     }
-    else if( i % 5 === 0 ) {
+    else if( num % 5 === 0 ) {
         console.log("buzz")
     }else{
-        console.log(i)
+        console.log(num)
     }
 }
